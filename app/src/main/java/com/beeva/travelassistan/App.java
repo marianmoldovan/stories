326516514553a781c37b158c80456bcba62a851b
Marian C Moldovan <marian.claudiu@beeva.com>
2016-07-04 18:10:47 +0200

    Forgot to remove an import from contact library

--- app/src/main/java/com/beeva/travelassistan/App.java
+++ app/src/main/java/com/beeva/travelassistan/App.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import android.content.Intent;
 import android.support.multidex.MultiDexApplication;
 
-import com.kontakt.sdk.android.common.KontaktSDK;
-
 public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
